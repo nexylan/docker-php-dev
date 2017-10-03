2ee1d53e7c32f205ba914229995d723921b198b3
Sullivan SENECHAL <soullivaneuh@gmail.com>
2017-10-03 18:58:34 +0200

    User setup fix for Apache/FPM under Debian

--- 7.0/fpm/alpine/setup.sh
+++ 7.0/fpm/alpine/setup.sh
@@ -3,15 +3,22 @@ set -e
 
 if [ ! "$(getent group ${UNIX_GROUP})" ]; then
     echo "Create group ${UNIX_GROUP}"
-    addgroup -g "${UNIX_GID}" "${UNIX_GROUP}"
+    if [ "$(which apk)" ]; then
+        addgroup -g "${UNIX_GID}" "${UNIX_GROUP}"
+    else
+        groupadd --gid "${UNIX_GID}" "${UNIX_GROUP}"
+    fi
 
     sed --in-place --expression="s/group = www-data/group = ${UNIX_GROUP}/g" /usr/local/etc/php-fpm.d/www.conf
 fi
 
 if [ ! "$(getent passwd "${UNIX_USERNAME}")" ]; then
     echo "Create user ${UNIX_USERNAME}"
-    adduser -u "${UNIX_UID}" "${UNIX_USERNAME}"
-    adduser "${UNIX_USERNAME}" "${UNIX_GROUP}"
+    if [ "$(which apk)" ]; then
+        adduser -D -h /home/developer -u "${UNIX_UID}" "${UNIX_USERNAME}" -G "${UNIX_GROUP}"
+    else
+        useradd --home /home/developer --uid "${UNIX_UID}" --gid "${UNIX_GID}" "${UNIX_USERNAME}"
+    fi
 
     sed --in-place --expression="s/user = www-data/user = ${UNIX_USERNAME}/g" /usr/local/etc/php-fpm.d/www.conf
 fi
